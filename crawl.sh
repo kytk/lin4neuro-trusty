beed76a8325044b3d0fdadfada4d636c16a9ad63
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2016-08-06 21:41:40 +0900

    add crawler scripts

---
+++ crawl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+firefox http://download.slicer.org/ &
+firefox -new-tab http://www.itksnap.org/pmwiki/pmwiki.php?n=Downloads.SNAP3 &
+firefox -new-tab http://ric.uthscsa.edu/mango/mango.html &
+firefox -new-tab https://www.nitrc.org/frs/?group_id=889 &
+firefox -new-tab http://www.nitrc.org/frs/?group_id=152 &
+firefox -new-tab https://afni.nimh.nih.gov/pub/dist/tgz/ &
+
+
